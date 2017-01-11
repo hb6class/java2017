c649a92d552f08f7403d0e65d8b57c98a33e2252
USER@hb6003 <hb.6class@gmail.com>
2017-01-11 17:45:29 +0900

    day03

--- day03/Ex08.java
+++ day03/Ex08.java
@@ -28,6 +28,47 @@ class Ex08{
       System.out.println("문자입니다");
     }
     
+  //  kor=90, eng=80, math=70
+  // -------------------------------------------
+  // 국어   영어    수학    총점    평균  학점
+  // -------------------------------------------
+  //    90    80      70      000    80.00    B
+  // -------------------------------------------
+  //  평균 90이상 A
+  //        80이상 B
+  //        70이상 C
+  //        60이상 D
+  //        60미만 F
+  int kor, eng, math, tot;
+  double avg;
+  char result;
+  kor=99;
+  eng=100;
+  math=100;
+  tot=kor+eng+math;
+  avg=tot*100/3/100.0;
+  result ='F';
+  switch((int)(avg/10)){
+  case 10:
+  case 9:
+    result='A';
+    break;
+  case 8:
+    result='B';
+    break;
+  case 7:
+    result='C';
+    break;
+  case 6:
+    result='D';
+    break;
+  }
+  
+    System.out.println("-------------------------------------------");
+    System.out.println("국어\t영어\t수학\t총점\t평균\t학점");
+    System.out.println("-------------------------------------------");
+    System.out.println(kor+"\t"+eng+"\t"+math+"\t"+tot+"\t"+avg+"\t"+result);
+    System.out.println("-------------------------------------------");
     
   }
 
