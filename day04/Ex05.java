fb8693a04be1449f260f47d9f5c916cf1c162bea
USER@hb6003 <hb.6class@gmail.com>
2017-01-12 15:54:35 +0900

    day04-Q1.~Q3.

---
+++ day04/Ex05.java
@@ -0,0 +1,51 @@
+class Ex05{
+
+  public static void main(String[] args){
+  // 1 2 3
+  // 1 2 3
+  // 1 2 3
+  
+  
+  // 1 2 3
+  // 4 5 6
+  // 7 8 9
+  /*
+    int num=1;
+    for(int n=0; n<3; n++){
+                for(int m=0; m<3; m++){
+                  System.out.print((num++)+" ");
+                }
+                System.out.println();
+    }
+  */
+  /*
+    for(int i=0; i<3; i++){
+    
+      for(int j=0; j<3; j++){
+        System.out.print(3*i+j+1+" ");
+      
+      }
+      System.out.println();
+    }
+  */
+  
+    for(int x=1; x<10; x++){
+        System.out.print(x+" ");
+        if(x%3==0){
+        System.out.println();
+        }
+    }
+  
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
