7956ff8311edf0c07474f99edca79a1b7f198eee
USER@hb6003 <hb.6class@gmail.com>
2017-01-16 10:50:22 +0900

    day06 - am

---
+++ day06/src/com/hb/am/Ex01.java
@@ -0,0 +1,73 @@
+package com.hb.am;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// 
+		System.out.println("println"); // ()안의 내용을 출력하고 개행
+		System.out.print("print"+'\n');	// ()안의 내용을 출력
+		// \n 개행(엔터)
+		// window - \r\n
+		System.out.println("----------------------------");	// ()안의 내용을 출력
+		// 기본 자료형(원시자료형)
+		// 숫자(정수타입)
+//			byte - -128~127
+//			short
+//			int	(4,기본자료타입)
+//			long(8)
+		// 숫자(실수타입) - 연산조심
+//			float	(4)
+//			double (8,기본자료타입)
+		// 문자
+		// 숫자로 저장-유니코드, 아스키코드(x)
+//			char
+//			''
+//			(char)65  '\u0000'
+//			문자열-""
+		// 논리
+//			boolean
+//			true / false
+		
+		// boolean < byte < short < char < int < long < float < double
+		
+		// 연산
+//		()
+//		++,--						----> +1, -1
+//		+,-,*,/,%					----> 오른쪽타입의 자료형
+//		< , > , <= , >= , == , !=	----> true/false
+//		&&, ||						----> true/false
+//		= , += , -= , *= , /= , %=
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day06/src/com/hb/am/Ex02.java
@@ -0,0 +1,52 @@
+package com.hb.am;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 제어문
+		/* │
+		 * ├조건문 (만약 ~~ 이라면 ~~해라)
+		 * │├if
+		 * ││├ if(조건)실행문;
+		 * ││├ if(조건)실행문(참); else 실행문(거짓);
+		 * ││└ if(조건1)실행문(1이 참);  
+		 * ││			else if(조건2) 실행문(2가 참);
+		 * ││			else if(조건3) 실행문(3가 참);
+		 * ││			else 실행문(거짓);
+		 * ││
+		 * │└switch(value){
+		 * │ 		case value1:	실행문; break;
+		 * │ 		case value2:	실행문; break;
+		 * │ 		case value3:	실행문; break;
+		 * │ 		default: 실행문;
+		 * │ 	}
+		 * │ 
+		 * └반복문 (몇번 반복하라)
+		 *   │
+		 *   ├for(초기값; 조건문; 증감식){
+		 *   │	실행문;
+		 *   │}
+		 *   │
+		 *   ├while(조건){
+		 *   │	실행문;
+		 *   │	증감식;
+		 *   │}
+		 *   │
+		 *   └do{
+		 * 		실행문;
+		 *     	증감식;
+		 * 		}while(조건문);
+		 */
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
